620fc5b6945033cd5fcf807ecede918fdb756648
Haoru <hxue@ucsd.edu>
2021-03-19 21:36:17 -0700

    changed group norm

--- models.py
+++ models.py
@@ -11,16 +11,22 @@ from torchvision.models.resnet import resnet18
 
 import numpy as np
 
+def init_layers(layer):
+    if isinstance(layer, nn.Conv2d) or isinstance(layer, nn.Linear):
+        nn.init.kaiming_normal_(layer.weight)
+        # nn.init.zeros_(layer.bias)
+
 class BaselineResNet(nn.Module):
     def __init__(self, num_output):
         super().__init__()
-        self.resizing = nn.UpsamplingBilinear2d(size=(254, 254))
+        # self.resizing = nn.UpsamplingBilinear2d(size=(224, 224))
         # self.feature_extractor = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=False)
         self.feature_extractor = resnet18(norm_layer=Norm_Layer)
         self.feature_extractor.fc =nn.Linear(self.feature_extractor.fc.in_features, num_output)
+        self.apply(init_layers)
 
     def forward(self, x):
-        x = self.resizing(x)
+        # x = self.resizing(x)
         x = self.feature_extractor(x)
         return x
 
@@ -29,7 +35,9 @@ class ResNetMainBranch(nn.Module):
         super().__init__()
         self.resnet = BaselineResNet(1000)
         self.relu = nn.ReLU()
-        self.fc = nn.Linear(1000, 58)
+        self.fc = nn.Linear(1000, 43)
+        self.apply(init_layers)
+
 
     def forward(self, x):
         x = self.resnet(x)
@@ -43,9 +51,9 @@ class ResNetTwoBranch(nn.Module):
         super().__init__()
         self.resnet = BaselineResNet(1000)
         self.relu = nn.ReLU()
-        self.fc_main = nn.Linear(1000, 58)
+        self.fc_main = nn.Linear(1000, 43)
         self.fc_auxillary = nn.Linear(1000, 4)
-
+        self.apply(init_layers)
 
 
     def forward(self, x):
@@ -58,7 +66,7 @@ class ResNetTwoBranch(nn.Module):
 class Norm_Layer(nn.Module):
     def __init__(self, num_channel):
         super().__init__()
-        self.group_norm = GroupNorm(8, num_channel)
+        self.group_norm = GroupNorm(num_channel // 2, num_channel)
 
     def forward(self, x):
         return self.group_norm(x)
